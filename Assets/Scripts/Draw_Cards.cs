813322f6761b0cda3ad965606637e1fca6804635
Burnie <W0410404@campus.nscc.ca>
2023-11-08 13:28:10 -0400

    Lose Screen now triggers when drawing cards
    Drawing and Flipping cards can now both cause you to win or lose.

--- Assets/Scripts/Draw_Cards.cs
+++ Assets/Scripts/Draw_Cards.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,13 @@ using UnityEngine;
 public class Draw_Cards : MonoBehaviour
 {
     public PlayerManager PlayerManager;
+    public WinScreen winScreen;
 
     public void OnClick()
     {
         Debug.Log("fsdjfjsds");
         PlayerManager.DealCards();
+        winScreen.Win();
+        Console.WriteLine("Win(); is working in Draw_Cards");
     }
 }
